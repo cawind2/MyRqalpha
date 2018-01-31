1c8133f1620bcb98d1f5afb774b3fc972086bb65
Kong Lan <konglan@hotmail.com>
2018-01-31 14:08:20 +0800

    d

--- TestPy.py
+++ TestPy.py
@@ -1,8 +1,14 @@
+# coding:utf-8
+from rqalpha.api import *
+from rqalpha import run_func
+
 # 在这个方法中编写任何的初始化逻辑。context对象将会在你的算法策略的任何方法之间做传递。
 def init(context):
     logger.info("init")
-    context.s1 = "000001.XSHE"
-    update_universe(context.s1)
+    context.s1 = "000423.XSHE"
+    context.s2 = "601800.XSHG"
+    context.stocks = [context.s1,context.s2]
+    update_universe(context.stocks)
     # 是否已发送了order
     context.fired = False
     context.cnt = 1
@@ -26,6 +32,33 @@ def handle_bar(context, bar_dict):
 
     # TODO: 开始编写你的算法吧！
     if not context.fired:
+        for stock in context.stocks:
+            order_percent(stock,0.5)
         # order_percent并且传入1代表买入该股票并且使其占有投资组合的100%
-        order_percent(context.s1, 1)
+        # order_percent(context.s1, 1)
         context.fired = True
+
+
+
+config = {
+    "base":{
+        "data_bundle_path":"d:/PythonProject/bundle/bundle",
+        "start_date":"2018-01-01",
+        "end_date":"2018-01-27",
+        "benchmark":"000300.XSHG",
+        "accounts":{
+            "stock":100000
+        }
+    },
+    "extra":{
+        "log_level":"verbose"
+    },
+    "mod":{
+        "sys_analyser":{
+            "enabled":True,
+            "plot":True
+        }
+    }
+}
+
+run_func(init=init,before_trading=before_trading, handle_bar=handle_bar,config=config)
